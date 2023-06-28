085b7d200b5591beda8eb7051e6b77ac3b01f582
Paulo Matos <paulojomatos@gmail.com>
2023-06-27 23:43:11 -0300

    add deque initial implementation

---
+++ Deque.java
@@ -0,0 +1,104 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+    private Node first;
+    private Node last;
+    int count = 0;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public class DequeIterator implements Iterator<Item> {
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    // construct an empty deque
+    public Deque() {}
+
+    // is the deque empty?
+    public boolean isEmpty() { return size() == 0; }
+
+    // return the number of items on the deque
+    public int size() {
+        return count;
+    }
+
+    // add the item to the front
+    public void addFirst(Item item) {
+        Node oldFirst = first;
+
+        Node newNode = new Node();
+        newNode.item = item;
+        newNode.next = oldFirst;
+
+        if (isEmpty()) {
+            last = newNode;
+        }
+
+        first = newNode;
+
+        count++;
+    }
+
+
+
+    // add the item to the back
+    public void addLast(Item item) {
+        Node newNode = new Node();
+        newNode.item = item;
+        newNode.next = null;
+
+        if (isEmpty()) {
+            first = newNode;
+        } else {
+            last.next = newNode;
+        }
+
+        last = newNode;
+
+        count++;
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst() { return null; }
+
+    // remove and return the item from the back
+    public Item removeLast() { return null; }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator() {
+        return new DequeIterator();
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+        Deque<Integer> deque = new Deque<>();
+
+        deque.addLast(1);
+        deque.addFirst(2);
+        deque.addLast(5);
+
+        System.out.println("size -> " + deque.size());
+
+        for (Integer item : deque) {
+            System.out.println("Item ->" + item);
+        }
+
+
+    }
+
+}
